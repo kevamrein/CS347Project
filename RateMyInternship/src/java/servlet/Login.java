bc31169f7bfc1daedeca09072238c26deee13a78
Kevin Amrein <amreinkh@dukes.jmu.edu>
2016-12-04 14:23:35 -0500

    Finally Working

---
+++ RateMyInternship/src/java/servlet/Login.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author kevinamrein
+ */
+@WebServlet(name = "Login", urlPatterns = {"/login"})
+public class Login extends HttpServlet {
+ /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/html");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if ((!Pattern.matches("^[a-z][a-z0-9]*$", username)) ||
+                (!Pattern.matches("^[a-z0-9!@#$*]*$", password)))
+        {
+            response.sendRedirect(request.getContextPath() + "/index.jsp");
+        }
+         response.sendRedirect(request.getContextPath() + "/index.jsp");     
+    }
+}
--- RateMyInternship/web/login.jsp
+++ RateMyInternship/web/login.jsp
@@ -44,7 +44,7 @@
         <script type="text/javascript" src="js/jquery.min.js"></script>
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
         
-         <form method="GET" name="login"/>
+         <form method="POST" action="login"/>
          Username: <input type="text" name="username"/>
         </br>
         Password: <input type="password" name="password"/>
