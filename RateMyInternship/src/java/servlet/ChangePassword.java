29381cfaeaf9a0b581d8d6b4b2f6c582f856f9a9
camsarswell <carswellsam@gmail.com>
2016-12-09 18:52:04 -0500

    Add files via upload

---
+++ RateMyInternship/src/java/servlet/ChangePassword.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+import database.*;
+import dataObjects.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author samcarswell
+ */
+@WebServlet(name = "ChangePassword", urlPatterns = {"/ChangePassword"})
+public class ChangePassword extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String answer = request.getParameter("answer");
+        String password = request.getParameter("password");
+        String confirm = request.getParameter("confirm");
+        String user_id;
+        String output = "";
+        
+        user_id = request.getSession().getAttribute("user_id").toString();
+        
+        if (!Query.getSecurityAnswer(user_id).equals(answer))
+        {
+            output = "Error: Answer does not match.";
+        } else if (!Utilities.isValidPassword(password))
+        {
+            output = "Error: Password contains invalid characters";
+        } else if (!password.equals(confirm))
+        {
+            output = "Error: Passwords do not match";
+        } else
+        {
+            User user = Query.getUser(user_id);
+            output = Query.editUserPassword(user_id, user.getPassword(), Utilities.hashPassword(password));
+            if (!output.contains("Error"))
+            {
+                request.getSession(true).setAttribute("signed_in", true);
+                request.getSession().setAttribute("username", user.getUsername());
+                request.getSession().setAttribute("user_id", user.getUserId());
+                response.sendRedirect(request.getContextPath() + "/index.jsp");
+            }
+        }
+        
+        
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ RateMyInternship/src/java/servlet/ForgetPassword.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import dataObjects.User;
+import database.Query;
+import database.Utilities;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author samcarswell
+ */
+@WebServlet(name = "forget_password", urlPatterns = {"/forget_password"})
+public class ForgetPassword extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text;charset=UTF-8");
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String output = "";
+        
+        User user = Query.getUserCreds(username);
+        
+        if (user == null)
+        {
+            output = "Error: User does not exist.";
+        } else if (!user.getEmail().equals(email))
+        {
+            output = "Error: Email does not match.";
+        } else
+        {
+            response.sendRedirect(request.getContextPath() + "/change_password.jsp");
+            request.getSession().setAttribute("user_id", user.getUserId());
+        }
+        
+        if (output.contains("Error"))
+        {
+            try (PrintWriter out = response.getWriter())
+            {
+                out.println(output);
+            }
+        }
+            
+            
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Servlet for Forget Password Page";
+    }// </editor-fold>
+
+}
